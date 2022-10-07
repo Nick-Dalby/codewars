3a940334b245f05b2ae8a1eb05c886381c14bb5f
Nick Dalby <nckdlby@gmail.com>
2022-10-07 10:12:05 +0200

    count milliseconds

---
+++ millisPastMidnight.js
@@ -0,0 +1,8 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// SOLUTION:
+function past(h, m, s){
+  return (s * 1000) + (m * 60000) + (h * 3600000)
+}
