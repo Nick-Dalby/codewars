3e7af68cbb3eef92b0b69068ee3d58dde3e8c578
Nick Dalby <nckdlby@gmail.com>
2022-10-13 09:35:13 +0200

    filter and reduce

--- countPosSumNeg.js
+++ countPosSumNeg.js
@@ -17,3 +17,7 @@ function countPositivesSumNegatives(input) {
   }
   return [count , sum];
 }
+
+function countPositivesSumNegatives(input) {
+  return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
