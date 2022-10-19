5da9cd871c5240abe9a74c963fe8deb15efb336d
Nick Dalby <nckdlby@gmail.com>
2022-10-19 09:24:02 +0200

    Math.min / Math.max

---
+++ highestProfit.js
@@ -0,0 +1,7 @@
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+
+// SOLUTION:
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)]
+}
