964613416454e25950220bcec2b8a157dbe09cbd
Nick Dalby <nckdlby@gmail.com>
2022-10-20 10:45:18 +0200

    ternary

---
+++ calculateBMI.js
@@ -0,0 +1,10 @@
+
+//SOLUTION:
+
+function bmi(weight, height) {
+  let bmi = weight / (height * height)
+  return  bmi <= 18.5 ? "Underweight"
+  : bmi <= 25 ? 'Normal'
+  : bmi <= 30 ? ' Overweight'
+  : ' Obese'
+}
