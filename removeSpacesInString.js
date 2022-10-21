c7796e817c69bda4ed7729d2bf55c01fe6d59eec
Nick Dalby <nckdlby@gmail.com>
2022-10-21 09:13:03 +0200

    split/join or regex replace

---
+++ removeSpacesInString.js
@@ -0,0 +1,13 @@
+//SOLUTION:
+
+function noSpace(x){
+  return x.replace(/ /g,'')
+}
+
+function noSpace(x){
+  return x.split(" ").join('')
+}
+
+function noSpace(x){
+  return x.replace(/\s/g, '')
+}
