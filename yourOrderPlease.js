f523fe7c25e65147d1425df6089cd779833727e3
Nick Dalby <nckdlby@gmail.com>
2022-10-24 10:00:29 +0200

    comment

--- yourOrderPlease.js
+++ yourOrderPlease.js
@@ -1,4 +1,4 @@
-/ /Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+//Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 
 
 
