bead0fc5656cb5bdd52c81b0f45871a983f1af3b
nick <nckdlby@gmail.com>
2022-11-11 11:07:53 +0100

    Math.floor()

--- growthOfPopulation.js
+++ growthOfPopulation.js
@@ -3,7 +3,7 @@
 function nbYear(p0, percent, aug, p) {
   let i = 0
   for (i; p0 < p; i++) {
-    p0 += p0 * (percent / 100) + aug
+    p0 += Math.floor(p0 * (percent / 100) + aug)
   }
   return i
 }
