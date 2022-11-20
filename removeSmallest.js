b919b2ea969a26c59614cad993c0105e45faee7b
nick <nckdlby@gmail.com>
2022-11-20 14:40:39 +0100

    Math.min(), slice(), splice()

---
+++ removeSmallest.js
@@ -0,0 +1,9 @@
+// SOLUTION:
+
+function removeSmallest(numbers) {
+  if (!numbers) return []
+  let smallest = Math.min(...numbers)
+  let newNums = numbers.slice(0)
+  newNums.splice(newNums.indexOf(smallest), 1)
+  return newNums
+}
